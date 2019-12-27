aaef4f3af9f490ee2ba447c93e20b7abad8795df
SPL\SRoy3 <suman.roy3@stryker.com>
2019-12-27 13:50:43 +0530

    Updating Shallow net on animals dataset.

--- cnn_keras/shallow_net_on_animals.py
+++ cnn_keras/shallow_net_on_animals.py
@@ -12,6 +12,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
+# Hyper parameters
+epochs = 100
+learning_rate = 0.001
+training_batch_size = 32
+predict_batch_size = 32
+decay_rate = learning_rate / epochs
+momentum = 0.8
+filters = 32
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
@@ -35,31 +44,33 @@ data = data.astype("float") / 255.0
 # the data for training and the remaining 25% for testing
 (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
 # convert the labels from integers to vectors
-trainY = LabelBinarizer().fit_transform(trainY)
-testY = LabelBinarizer().fit_transform(testY)
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
 
 # initialize the optimizer and model
 print("[INFO] compiling model...")
-opt = SGD(lr=0.005)
-model = shallow_net.ShallowNet.build(width=32, height=32, depth=3, classes=3)
+# opt = SGD(lr=learning_rate)
+opt = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
+model = shallow_net.ShallowNet.build(width=32, height=32, depth=3, classes=3, filters=filters)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # train the network
 print("[INFO] training network...")
-H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=1)
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=training_batch_size, epochs=epochs, verbose=1)
 
 # evaluate the network
 print("[INFO] evaluating network...")
-predictions = model.predict(testX, batch_size=32)
+predictions = model.predict(testX, batch_size=predict_batch_size)
 print(classification_report(testY.argmax(axis=1),predictions.argmax(axis=1), target_names=["cat", "dog", "panda"]))
 
 # plot the training loss and accuracy
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_acc")
+plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
