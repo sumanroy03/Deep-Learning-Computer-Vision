e9d7fd31303a5a59528a372e997af375ec1a81ff
SPL\SRoy3 <suman.roy3@stryker.com>
2020-01-04 23:31:06 +0530

    Creating miniVggNet models over CIFAR-10 and animals dataset.

---
+++ mini_vgg_net/minivggnet_animals.py
@@ -0,0 +1,83 @@
+# import the necessary packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from computer_vision.preprocessing import ImageToArrayPreprocessor
+from computer_vision.preprocessing import SimplePreprocessor
+from computer_vision.datasets import SimpleDatasetLoader
+from computer_vision.nn.conv import mini_vgg_net
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+# Hyper parameters
+epochs = 40
+learning_rate = 0.01
+training_batch_size = 32
+predict_batch_size = 32
+decay_rate = learning_rate / epochs
+momentum = 0.9
+# filters = 32
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
+ap.add_argument("-m", "--model", required=True, help="path to output model")
+args = vars(ap.parse_args())
+
+# grab the list of images that we’ll be describing
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the image preprocessors
+sp = SimplePreprocessor.SimplePreprocessor(32, 32)
+iap = ImageToArrayPreprocessor.ImageToArrayPreprocessor()
+
+# load the dataset from disk then scale the raw pixel intensities
+# to the range [0, 1]
+sdl = SimpleDatasetLoader.SimpleDatasetLoader(preprocessors=[sp, iap])
+(data, labels) = sdl.load(imagePaths, verbose=500)
+data = data.astype("float") / 255.0
+
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
+# convert the labels from integers to vectors
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+# initialize the optimizer and model
+print("[INFO] compiling model...")
+# opt = SGD(lr=learning_rate)
+opt = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
+model = mini_vgg_net.MiniVGGNet.build(width=32, height=32, depth=3, classes=3)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+# train the network
+print("[INFO] training network...")
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=training_batch_size, epochs=epochs, verbose=1)
+
+# save the network to disk
+print("[INFO] serializing network...")
+model.save(args["model"])
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=predict_batch_size)
+print(classification_report(testY.argmax(axis=1),predictions.argmax(axis=1), target_names=["cat", "dog", "panda"]))
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.show()
